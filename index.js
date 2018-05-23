d8cb0075c2aafcdcd34ec6a6b64b130a422d10df
Trevor Jameson <tjameson15@gmail.com>
2018-05-23 01:07:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,31 @@ const app = "I don't do much."
 function destructivelyAppendKitten(name) {
   kittens = [...kittens, name];
 }
+
+function destructivelyPrependKitten(name) {
+  kittens = [name, ...kittens];
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var arr2 = kittens;
+  arr2 = [...arr2, kittens];
+  return arr2;
+}
+
+function prependKittens(name) {
+  var arr2 = kittens;
+  arr2 = [kittens, ...arr2];
+  return arr2;
+}
+
+function removeLastKitten() {
+  kitten.pop()
+}
